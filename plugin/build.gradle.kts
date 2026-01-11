2a98b4d0be74dfcf374cd20fec962350a22987e6
siba-dev <commits@siba.dev>
2026-01-12 00:37:17 +0100

    Configured publishing

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
 
     // Apply the Kotlin JVM plugin to add support for Kotlin.
     alias(libs.plugins.kotlin.jvm)
+
+    id("com.gradle.plugin-publish") version "2.0.0"
 }
 
 group = "de.siba"
@@ -19,6 +21,20 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+gradlePlugin {
+    website = "https://github.com/siba-dev/functional-test-gradle-plugin"
+    vcsUrl = "https://github.com/siba-dev/functional-test-gradle-plugin.git"
+
+    // Define the plugin
+    val plugin by plugins.creating {
+        id = "$group.functional-test"
+        implementationClass = "de.siba.functionaltest.FunctionalTestGradlePlugin"
+        displayName = "Functional Test Gradle Plugin"
+        description = "Convenience plugin adding functional tests to Gradle plugin projects."
+        tags = listOf("testing", "integrationTesting")
+    }
 }
 
 tasks.withType<Test> {
