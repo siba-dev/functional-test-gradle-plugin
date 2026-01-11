2103b9436818195c4ad99ac6fcb2ec6ec20aa4c9
siba-dev <commits@siba.dev>
2026-01-12 00:33:45 +0100

    Implemented functionalTest definition

--- plugin/src/main/kotlin/de/siba/functionaltest/FunctionalTestGradlePlugin.kt
+++ plugin/src/main/kotlin/de/siba/functionaltest/FunctionalTestGradlePlugin.kt
@@ -8,5 +8,33 @@ import org.gradle.plugin.devel.GradlePluginDevelopmentExtension
 
 class FunctionalTestGradlePlugin : Plugin<Project> {
     override fun apply(project: Project) {
+        project.plugins.apply("java-gradle-plugin")
+
+        val javaExtension = project.extensions.getByType(JavaPluginExtension::class.java)
+
+        // Add a source set for the functional test suite
+        val functionalTestSourceSet = javaExtension.sourceSets.create("functionalTest")
+        project.configurations.getByName("functionalTestImplementation").extendsFrom(
+            project.configurations.getByName("testImplementation")
+        )
+        project.configurations.getByName("functionalTestRuntimeOnly").extendsFrom(
+            project.configurations.getByName("testRuntimeOnly")
+        )
+
+        project.extensions.getByType(GradlePluginDevelopmentExtension::class.java).testSourceSets.add(
+            functionalTestSourceSet
+        )
+
+        // Add a task to run the functional tests
+        val functionalTest = project.tasks.register("functionalTest", Test::class.java) { task ->
+            task.testClassesDirs = functionalTestSourceSet.output.classesDirs
+            task.classpath = functionalTestSourceSet.runtimeClasspath
+            task.useJUnitPlatform()
+        }
+
+        project.tasks.named("check") { task ->
+            // Run the functional tests as part of `check`
+            task.dependsOn(functionalTest)
+        }
     }
 }
--- plugin/src/test/kotlin/de/siba/functionaltest/FunctionalTestGradlePluginTest.kt
+++ plugin/src/test/kotlin/de/siba/functionaltest/FunctionalTestGradlePluginTest.kt
@@ -10,4 +10,38 @@ import kotlin.test.Test
 import kotlin.test.assertNotNull
 
 class FunctionalTestGradlePluginTest {
+    @Test
+    fun sourceSetPresent() {
+        val project = createProject()
+
+        val javaExtension = project.extensions.getByType(JavaPluginExtension::class.java)
+        val developmentExtension = project.extensions.getByType(GradlePluginDevelopmentExtension::class.java)
+
+        assertNotNull(
+            javaExtension.sourceSets.findByName("functionalTest"), "Could not find source set."
+        )
+        assertNotNull(developmentExtension.testSourceSets.find {
+            it.name == "functionalTest"
+        }, "Test source set not registered.")
+    }
+
+    private fun createProject(): Project {
+        val project = ProjectBuilder.builder().build()
+        project.plugins.apply("de.siba.functional-test")
+        return project
+    }
+
+    @Test
+    fun taskRegistered() {
+        val project = createProject()
+
+        assertNotNull(
+            project.tasks.findByName("functionalTest"), "Functional test task not registered."
+        )
+
+        val checkTask = project.tasks.getByName("check")
+        assertNotNull(
+            checkTask.taskDependencies.getDependencies(checkTask)
+                .find { it.name == "functionalTest" })
+    }
 }
